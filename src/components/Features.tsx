e51f6698a04fc9e1ddd200c93192eae617e106d2
Damm Bro <144452060+YashDhirajOza@users.noreply.github.com>
2024-09-15 23:49:21 +0530

    Add files via upload

---
+++ src/components/Features.tsx
@@ -0,0 +1,27 @@
+import Bentodemo from './bentogrid';
+
+export const Features = () => {
+  return (
+
+    <div className="bg-black text-white py-[72px] sm:py-24 ">
+
+      <div className="container">
+      <h2 className="text-center font-bold text-5xl sm:text-6xl tracking-tighter">Everything You Need</h2>
+<div className='max-w-xl mx-auto'>
+  <p className="text-center mt-5 text-xl text-white/70">
+    Experience unparalleled solar efficiency with our advanced tracking system. It seamlessly follows the sun path, delivering maximum energy output and transforming the way you harness solar power.
+  </p>
+</div>
+
+        <div className="flex flex-col items-center justify-center sm:flex-row gap-4 mt-32">
+          <Bentodemo />
+          
+
+        </div>
+
+      </div>
+
+
+    </div>
+  )
+}
